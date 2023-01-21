3c9f15123fa4d4a8262390833e5f98d258891582
Aziz1019 <starinc1998@gmail.com>
2023-01-21 12:47:17 +0500

    Initial Commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float number = toBeDivided / 5.0f;
+        System.out.println(number);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider divider = new NumberDivider();
+        divider.divide(19);
     }
 }
